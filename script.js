0d6a5e0cd877a6ea854ca8c83924f85ba7161f7f
aftab-alam02526 <aftab0447@gmail.com>
2025-09-26 14:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let angle = 0; // initial rotation angle
+const line = document.getElementById('line');
+
+setInterval(() => {
+  angle += 2; // increase angle by 2 degrees
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`; 
+  // translate(-50%, -50%) keeps the line centered
+}, 20);
